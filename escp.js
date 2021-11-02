0cd044524da1e5316b01d0ca061b20d184c2b95a
Ibnu Syifa Takbir Adha <69663123+Ibnustagnz@users.noreply.github.com>
2021-11-02 08:09:00 -0700

    Create escp.js

---
+++ escp.js
@@ -0,0 +1,70 @@
+function show_loading(){
+  document.getElementById("loadings").style.display="block";
+}
+function hide_loading(){
+  document.getElementById("loadings").style.display="none";
+  let hideEffect=setInterval(()=>{
+    if(!document.getElementById("loadings").style.opacity){
+      document.getElementById("loadings").style.opacity="1";
+    }
+    if(document.getElementById("loadings").style.opacity>"0"){
+      document.getElementById("loadings").style.opacity="0.1";
+    }
+    else{
+      clearInterval(hideEffect);
+      document.getElementById("loadings").style.display="none";
+    }
+  },100);
+}
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // Show backToTopButton
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else { // Hide backToTopButton
+    if(backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 200;
+  let start = null;
+  
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
